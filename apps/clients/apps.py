b587494d3b689f9ea73dc095d68a883fb5ff70c3
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2026-02-21 23:56:08 -0300

    Refactor: Purge defensive language + correct accents (Développé, basé, protégé, confidentialité)

--- apps/clients/apps.py
+++ apps/clients/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class ClientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.clients'
-    verbose_name = 'Espace Client'
+    verbose_name = 'Écosystème TUS'
     
     def ready(self):
         """Import signals when app is ready."""
